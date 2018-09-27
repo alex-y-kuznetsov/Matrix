4fda3db5623dd8280f7578723e16517d3f3fad53
Alex Kuznetsov <alex.y.kuznetsov@gmail.com>
2018-09-28 00:23:22 +0500

    добавил функцию получения случайного цвета

--- build/js/domain.js
+++ build/js/domain.js
@@ -1,7 +1,14 @@
 'use strict';
 
 (function () {
+  var getRandomColor = function () {
+    var r = Math.floor(Math.random() * 256);
+    var g = Math.floor(Math.random() * 256);
+    var b = Math.floor(Math.random() * 256);
+    return '#' + r.toString(16) + g.toString(16) + b.toString(16);
+  };
 
+  var usedColors = [];
   window.colorOnes = function () {
     if (matrixHome.hasChildNodes()) {
       var matrixRows = matrixHome.rows.length;
@@ -9,10 +16,16 @@
         var matrixCells = matrixHome.rows[i].cells.length;
         for (var j = 0; j < matrixCells; j++) {
           if (matrixHome.rows[i].cells[j].innerHTML === '1') {
-            matrixHome.rows[i].cells[j].style.backgroundColor = 'red';
+            var cellColor = matrixHome.rows[i].cells[j].style.backgroundColor = getRandomColor();
+            usedColors.push(cellColor);
+            // if (matrixHome.rows[i-1].cells[j].textContent.includes('1')) {
+            //   matrixHome.rows[i-1].cells[j].style.backgroundColor = cellColor;
+            // }
+          } else {
+            var cellColor = matrixHome.rows[i].cells[j].style.backgroundColor = '#FFFFFF';
           }
         }
       }
     }
-  }
+  };
 })();
