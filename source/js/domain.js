fe74e1614c2a6a2ed7717db480d7d48c825915b5
Alex Kuznetsov <alex.y.kuznetsov@gmail.com>
2018-09-28 14:14:49 +0500

    получил массив ячеек с единицами

--- source/js/domain.js
+++ source/js/domain.js
@@ -8,6 +8,25 @@
     return '#' + r.toString(16) + g.toString(16) + b.toString(16);
   };
 
+  // Проверяем, есть ли у ячейки с "1" соседи с "1"
+  var neighbours = [];
+  var checkNeighbours = function () {
+    if (matrixHome.hasChildNodes()) {
+      var matrixCells = document.querySelectorAll('.matrix-cell');
+      var matrixCellsWithOne = [];
+      for (var a = 0; a < matrixCells.length; a++) {
+        if (matrixCells[a].textContent.includes('1')) {
+          matrixCellsWithOne.push(matrixCells[a]);
+        }
+        // var matrixCellWithOne = matrixCells[a].textContent.includes('1');
+        // matrixCellsWithOne.push(matrixCellWithOne);
+      }
+      console.log(matrixCellsWithOne);
+    }
+  };
+
+  // Выбираем рандомный цвет, проверяем, нет ли в массиве использованных цветов такого цвета,
+  // красим все смежные ячейки в этот цвет и пушим цвет в масси использованных цветов
   var usedColors = [];
   window.colorOnes = function () {
     if (matrixHome.hasChildNodes()) {
@@ -18,9 +37,9 @@
           if (matrixHome.rows[i].cells[j].innerHTML === '1') {
             var cellColor = matrixHome.rows[i].cells[j].style.backgroundColor = getRandomColor();
             usedColors.push(cellColor);
-            // if (matrixHome.rows[i-1].cells[j].textContent.includes('1')) {
-            //   matrixHome.rows[i-1].cells[j].style.backgroundColor = cellColor;
-            // }
+            if (checkNeighbours(i+1, j+1)) {
+              matrixHome.rows[i+1].cells[j+1].style.backgroundColor = cellColor;
+            }
           } else {
             var cellColor = matrixHome.rows[i].cells[j].style.backgroundColor = '#FFFFFF';
           }
